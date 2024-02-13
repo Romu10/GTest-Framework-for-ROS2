0e3b46e8b5d74afe789181fdff61a64557f4f7cf
Romu10 <romulobryanp@gmail.com>
2024-02-13 23:18:44 +0000

    printing goal information

--- tortoisebot_waypoints/src/tortoisebot_action_server.cpp
+++ tortoisebot_waypoints/src/tortoisebot_action_server.cpp
@@ -8,6 +8,7 @@
 #include "std_msgs/msg/detail/empty__struct.hpp"
 #include "std_msgs/msg/empty.hpp"
 #include "nav_msgs/msg/odometry.hpp"
+#include <cmath>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/utils.h>
 #include "waypoints_interfaces/action/waypoint_action.hpp"
@@ -42,16 +43,15 @@ public:
 
 private:
 
-    rclcpp_action::Server<TortoisebotWaypoints>::SharedPtr action_server_;
-
     rclcpp_action::GoalResponse handle_goal(
         const rclcpp_action::GoalUUID &uuid,
         std::shared_ptr<const TortoisebotWaypoints::Goal> goal)
     {
-        RCLCPP_INFO(this->get_logger(), "Goal received: \nx: %f \ny: %f", goal->position.x, goal->position.y);
         (void)uuid;
 
-        // Your goal handling logic here
+        des_pos.x = goal->position.x;
+        des_pos.y = goal->position.y;
+        RCLCPP_INFO(this->get_logger(), "Goal received: \nx: %f \ny: %f", des_pos.x, des_pos.y);
 
         return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     }
@@ -80,6 +80,15 @@ private:
     void execute(const std::shared_ptr<GoalHandleTortoisebotWaypoints> goal_handle)
     {
         RCLCPP_INFO(this->get_logger(), "Executing goal");
+
+        // Define desired position and erros
+        desired_yaw = std::atan2(des_pos.y - cur_pos.y, des_pos.x - cur_pos.x);
+        err_pos = std::sqrt(std::pow(des_pos.y - cur_pos.y, 2) + std::pow(des_pos.x - cur_pos.x, 2));
+        err_yaw = desired_yaw - current_yaw; 
+
+        // Printing 
+        RCLCPP_INFO_ONCE(this->get_logger(), "Goal information: \nDesired yaw: %f \nError yaw: %f \nError position: %f", desired_yaw, err_yaw, err_pos);
+
     }
 
     long double calculateYaw(long double qx, long double qy, long double qz, long double qw) {
@@ -91,7 +100,7 @@ private:
     void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
         // Getting robot's current position
-        current_position = msg->pose.pose.position; 
+        cur_pos = msg->pose.pose.position; 
 
         // Calculate Yaw
         quaternion_x = msg->pose.pose.orientation.x;
@@ -105,27 +114,38 @@ private:
         quaternion.w = quaternion_w;
 
         calculate_yaw = calculateYaw(quaternion_x, quaternion_y, quaternion_z, quaternion_w);
-        yaw_degree = (calculate_yaw * (180.0 / M_PI));
+        current_yaw = (calculate_yaw * (180.0 / M_PI));
 
         if (!msg){
             RCLCPP_ERROR(this->get_logger(), "No data received from topic: /odom");
         }
         else{
-            RCLCPP_INFO_ONCE(this->get_logger(), "Data received from topic /odom: \nYaw: %f", yaw_degree);
+            RCLCPP_INFO_ONCE(this->get_logger(), "Data received from topic /odom: \nYaw: %f", current_yaw);
         }
 
     }
 
+
+    rclcpp_action::Server<TortoisebotWaypoints>::SharedPtr action_server_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
 
-    // Variables 
-    geometry_msgs::msg::Point current_position;
+    // Pos related 
+    geometry_msgs::msg::Point cur_pos;
+    geometry_msgs::msg::Point des_pos; 
+
+    // Yaw related
+    double calculate_yaw; 
+    double current_yaw; 
+    double desired_yaw; 
+
+    // Calculate Yaw related
     geometry_msgs::msg::Quaternion quaternion;
     long double quaternion_x, quaternion_y, quaternion_z, quaternion_w = 0.0;  
-    double calculate_yaw; 
-    double yaw_degree;
 
+    // Error related
+    double err_pos; 
+    double err_yaw;
 
 };
 
